c40fd52073f9cb5ba5dbdf69cf068b8426829463
Brian Watkins <brian.watkins@gmail.com>
2021-06-26 09:09:34 -0400

    Reporter tests against BDVP document
    - instead of a text report that could get out of
    sync with the real report format

--- packages/bdvp-reporter/spec/TestableApp.ts
+++ packages/bdvp-reporter/spec/TestableApp.ts
@@ -1,16 +1,20 @@
+import { validate, Writer, Behavior, Context } from "bdvp"
 import { Readable, Writable } from "stream"
 import { BDVPReporter } from "../src/BDVPReporter"
+
+export const appContext: Context<TestableApp> = { init: () => new TestableApp() }
 
 export class TestableApp {
   private bdvpReporter = new BDVPReporter()
   private consumer = new OutputConsumer()
 
-  async executeDoc(document: string): Promise<void> {
-    const lines: Array<string> = document.match(/^.*(\n|$)/gm) ?? []
-    const readableDoc = Readable.from(lines)
-    readableDoc.pipe(this.bdvpReporter).pipe(this.consumer)
+  async validateBehavior(behavior: Behavior): Promise<void> {
+    const reportWriter = new ReportWriter()
+    await validate([ behavior ], { writer: reportWriter })
+    const reportStream = reportWriter.stream()
+    reportStream.pipe(this.bdvpReporter).pipe(this.consumer)
     return new Promise(resolve => {
-      readableDoc.on("close", () => {
+      reportStream.on("close", () => {
         resolve()
       })
     })
@@ -21,11 +25,25 @@ export class TestableApp {
   }
 }
 
+class ReportWriter implements Writer {
+  private lines: Array<string> = []
+
+  writeLine(message: string): void {
+    this.lines.push(`${message}\n`)
+  }
+
+  stream(): Readable {
+    const readable = Readable.from(this.lines)
+    return readable
+  }
+
+}
+
 class OutputConsumer extends Writable {
   public output: string = ""
 
   _write(chunk: any, encoding: string, next: (error?: Error) => void) {
-    this.output += chunk.toString() + "\n"
+    this.output += chunk.toString()
     next()
   }
 }
--- packages/bdvp-reporter/spec/output.doc.ts
+++ packages/bdvp-reporter/spec/output.doc.ts
@@ -1,36 +1,36 @@
 import { example, condition, effect, behavior } from 'bdvp'
 import expect from 'proclaim'
-import { TestableApp } from './TestableApp'
+import { appContext } from './TestableApp'
 
-const standardDocument = `
-TAP version 13
-# a sample spec
-# comparing some numbers
-# when something happens
-ok it compares two numbers
-1..1
-# valid observations: 1
-# invalid observations: 0
-# skipped: 0
-`
+const simpleBehavior =
+  behavior("A Sample Behavior", [
+    example()
+      .description("Comparing some numbers")
+      .script({
+        observe: [
+          effect("it compares two numbers", () => {
+            expect.equal(7, 7)
+          })
+        ]
+      })
+  ])
 
-const testableAppSubject = { init: () => new TestableApp() }
 
 const standardScenario =
-  example(testableAppSubject)
-    .description("standard document with scenario and observations")
+  example(appContext)
+    .description("simple behavior with example")
     .script({
       assume: [
-        condition("a document is executed", async (testableApp) => {
-          await testableApp.executeDoc(standardDocument)
+        condition("a behavior is validated", async (testableApp) => {
+          await testableApp.validateBehavior(simpleBehavior)
         })
       ],
       observe: [
-        effect("the report includes the name of the document", (testableApp) => {
-          expect.include(testableApp.output, "a sample spec")
+        effect("the report includes the name of the behavior", (testableApp) => {
+          expect.include(testableApp.output, "A Sample Behavior")
         }),
         effect("the report includes the name of the example", (testableApp) => {
-          expect.include(testableApp.output, "comparing some numbers")
+          expect.include(testableApp.output, "Comparing some numbers")
         })
       ]
     })
