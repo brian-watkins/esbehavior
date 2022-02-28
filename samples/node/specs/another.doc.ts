dcfd46b61a1d04e2e6730c905d28a008433cf5d3
Brian Watkins <brian.watkins@gmail.com>
2022-02-28 06:18:38 -0500

    added more claims to the sample node spec

--- samples/node/specs/another.doc.ts
+++ samples/node/specs/another.doc.ts
@@ -1,16 +1,61 @@
 import { behavior, condition, effect, example, skip } from 'esbehavior'
-import { Thing } from '../src/Thing.js'
+import { div, mod, sum } from "../src/math.js"
 import assert from 'proclaim'
 
 export default
-  behavior("another spec", [
-    example({ init: () => new Thing() })
-      .description("basic example")
+  behavior("math", [
+    example()
+      .description("sum")
       .script({
         observe: [
-          effect("the thing works", (thing) => {
-            assert.equal(thing.stuff(), "Stuff!")
+          effect("it is a function", () => {
+            assert.isFunction(sum)
           }),
+          effect("it adds numbers", () => {
+            assert.equal(sum(1, 2), 3)
+          }),
+          effect("it adds negative numbers", () => {
+            assert.equal(sum(-1, -2), -3)
+          }),
+          effect("it sums to zero", () => {
+            assert.equal(sum(-1, 1), 0)
+          })
+        ]
+      }),
+    example()
+      .description("divide")
+      .script({
+        observe: [
+          effect("it is a function", () => {
+            assert.isFunction(div)
+          }),
+          effect("it divides positive numbers", () => {
+            assert.equal(div(1, 2), 0.5)
+          }),
+          effect("it divides negative numbers", () => {
+            assert.equal(div(-1, -2), 0.5)
+          }),
+          effect("it divides by 1", () => {
+            assert.equal(div(-1, 1), -1)
+          })
+        ]
+      }),
+    example()
+      .description("mod")
+      .script({
+        observe: [
+          effect("it is a function", () => {
+            assert.isFunction(mod)
+          }),
+          effect("it mods positive numbers", () => {
+            assert.equal(mod(1, 2), 1)
+          }),
+          effect("it mods negative numbers", () => {
+            assert.equal(mod(-3, -2), -1)
+          }),
+          effect("it mods other numbers", () => {
+            assert.equal(mod(7, 4), 3)
+          })
         ]
       })
   ])
---
+++ samples/node/src/math.ts
@@ -0,0 +1,3 @@
+export function sum(a: number, b: number) { return a + b };
+export function div(a: number, b: number) { return a / b };
+export function mod(a: number, b: number) { return a % b };
